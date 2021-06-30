5a3d6b15ea10f7bb4c793b24b61a02c3c659094c
gonul2830 <pirisauruspdf@gmail.com>
2021-06-30 09:11:22 +0300

    projeye ilk adim

--- src/test/java/tests/ilkCalisma/ilkClass.java
+++ src/test/java/tests/ilkCalisma/ilkClass.java
@@ -1,4 +1,13 @@
 package tests.ilkCalisma;
 
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class ilkClass {
+
+@Test
+    public void test01(){
+    Driver.getDriver().get(ConfigReader.getProperty("kr_url"));
+}
 }
