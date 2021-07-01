6b05d406397fd274f70a65455891e22113297e89
emregdk1 <unixman.19077@gmail.com>
2021-07-01 17:24:29 +0300

    emre

--- src/test/java/tests/ilkCalisma/Deneme.java
+++ src/test/java/tests/ilkCalisma/Deneme.java
@@ -1,8 +1,13 @@
 package tests.ilkCalisma;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 public class Deneme {
 
     public static void main(String[] args) {
         System.out.println("deneme 1-2");
+
+        System.out.println("ben geldim");
     }
+
 }
