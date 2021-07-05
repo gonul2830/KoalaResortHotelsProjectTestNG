810bbcc62025d64571ee4b12d08248d3e847ab75
Zeki <zekierdemli80@gmail.com>
2021-07-05 16:32:54 +0200

    sorun cozuldu

--- src/test/java/pages/KoalaResortPage.java
+++ src/test/java/pages/KoalaResortPage.java
@@ -107,4 +107,84 @@ public class KoalaResortPage {
 
 
 
+    @FindBy (xpath = "//*[text()='Hotel Management']")
+    public WebElement hotelManagement;
+
+    @FindBy (partialLinkText = "Hotel List")
+    public WebElement hotelList;
+
+    @FindBy (xpath = "//*[text()='Add Hotel ']")
+    public WebElement addHotel;
+
+    @FindBy(xpath = "//input[@id='Code']")
+    public WebElement codeTextBox;
+
+    @FindBy(xpath = "//input[@id='Name']")
+    public WebElement nameTextBox;
+
+    @FindBy(xpath = "//input[@id='Address']")
+    public WebElement adressTextBox;
+
+    @FindBy(xpath = "//input[@id='Phone']")
+    public WebElement phoneTextBox;
+
+    @FindBy(xpath = "//input[@id='Email']")
+    public WebElement emailTextBox;
+
+    @FindBy(id = "IDGroup")
+    public WebElement acilirMenu;
+
+    @FindBy(id = "btnSubmit")
+    public WebElement saveButonu;
+
+    @FindBy(xpath = "//div[text()='Hotel was inserted successfully']")
+    public WebElement sonucYazisiElementi;
+
+    @FindBy(xpath = "//*[text()='OK']")
+    public WebElement sonOKButonu;
+//
+    @FindBy(partialLinkText = "Hotel Rooms")
+    public WebElement hotelRoomsBtn;
+
+    @FindBy(xpath = "//*[text()='Add Hotelroom ']")
+    public WebElement addHotelsRoom;
+
+    @FindBy(id = "IDHotel")
+    public WebElement idHotelDropDown;
+
+    @FindBy(xpath = "//textarea[@title='Zengin Metin Editörü, Description']")
+    public WebElement descriptionsTextBox;
+
+    @FindBy(id = "IDGroupRoomType")
+    public WebElement roomTypeDropdown;
+
+    @FindBy(id = "MaxAdultCount")
+    public WebElement maxAdult;
+
+    @FindBy(xpath = "//input[@name='Phone']")
+    public WebElement phoneTextTable;
+
+    @FindBy(xpath  ="//*[text()='Search']")
+    public WebElement hotelListSearchBtn;
+
+    @FindBy(xpath = "//tbody//tr//td[3]")
+    public WebElement nameHotelKontrol;
+
+    @FindBy(xpath = "//input[@name='Email']")
+    public WebElement emailTextTable;
+
+    @FindBy(xpath = "//input[@name='Name']")
+    public WebElement nameTextTable;
+
+    @FindBy(xpath = "//input[@name='Code']")
+    public WebElement codeTextTable;
+
+    @FindBy(xpath = "//*[text()='Room reservations']")
+    public WebElement roomReservation;
+
+
+
+
+
+
 }
